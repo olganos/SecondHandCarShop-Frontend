0c4ce50aac9cb4821644b7e555d498d3f9bbc84d
Olga Nosova <semverst@yandex.ru>
2022-01-21 23:24:52 +0300

    Do cars transformation

---
+++ src/Pages/Catalog/transformCars.js
@@ -0,0 +1,15 @@
+export const transformCars = (carResponse) => {
+    return carResponse.reduce((accumulator, currentValue) => {
+        return accumulator.concat(currentValue.cars.vehicles.map((vehicle) => {
+            return {
+                warehouse: {
+                    _id: currentValue._id,
+                    name: currentValue.name,
+                    location: currentValue.location,
+                    locationName: currentValue.cars.location,
+                },
+                ...vehicle
+            }
+        }))
+    }, [])
+}
